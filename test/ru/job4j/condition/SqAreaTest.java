7228fc2c34d6d1d58cde9818abcbb31aae8dfaa4
Kostyantyn-Chyzhyk <chyzhyk28@gmail.com>
2020-04-10 20:45:32 +0200

    4. Тесты для стороны прямоугольника.[#266800]

--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -3,16 +3,14 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PointTest {
+public class SqAreaTest {
 
     @Test
     public void whenManWeight() {
-        int x1 = 0;
-        int x2 = 2;
-        int y1 = 0;
-        int y2 = 0;
+        int k = 2;
+        int p = 6;
         double expected = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
 
