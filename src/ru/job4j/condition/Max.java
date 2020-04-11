6cdfbd4fa215fc2616ba257a8316fc44db691107
Kostyantyn-Chyzhyk <chyzhyk28@gmail.com>
2020-04-11 23:35:18 +0200

    4.3.1. А существует ли треугольник?[#266713]

--- src/ru/job4j/condition/Max.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.condition;
-
-public class Max {
-    public static int max(int left, int right) {
-        boolean result = left >= right;
-        return result ? left : right;
-    }
-}
