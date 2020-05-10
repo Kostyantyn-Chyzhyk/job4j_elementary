65a6642709bc0f7a9776d74c47cae82456254523
Kostyantyn-Chyzhyk <kostyantyn.chyzhyk@marconirovereto.it>
2020-05-10 08:37:56 +0200

    Exercise

---
+++ test/ru/job4j/sort/Merge.java
@@ -0,0 +1,26 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        for (int i = 0; i < left.length; i++) {
+            rsl[i] = left[i];
+        }
+        for (int i = 0; i < right.length; i++) {
+            rsl[i+left.length] = right[i];
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
